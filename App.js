4e71888c8bc94a057a323683b194d977ab2fb80a
Victor Emanuel <78376383+vicnuel@users.noreply.github.com>
2023-06-18 21:31:34 -0300

    teste

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>A vida é um porre que você toma sem querer</Text>
       <StatusBar style="auto" />
     </View>
   );
